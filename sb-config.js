4e17ee58b9ae3aea8b68331dbf9282eaa0470575
yazanalawaad3-stack <yazanalawaad3@gmail.com>
2025-12-17 21:10:07 +0200

    Delete sb-config.js

--- sb-config.js
+++
@@ -1,9 +0,0 @@
-// Central Supabase config (single source of truth)
-(function () {
-  var url = "https://oyowsjjmaesspqiknvhp.supabase.co";
-  var anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95b3dzamptYWVzc3BxaWtudmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MTE4NzcsImV4cCI6MjA4MTM4Nzg3N30.aBo32xNG_dh1QD7NBI4N6jhYFLY42Xyxer2DNXxJi-w";
-  window.SB_CONFIG = { url: url, anonKey: anonKey };
-  window.SUPABASE_URL = url;
-  window.SUPABASE_KEY = anonKey;
-  window.SUPABASE_ANON_KEY = anonKey;
-})();
